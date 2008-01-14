8402db7c7001606c5d0da6cfb9b37104a94d14a5
jonathan.hess@gmail.com <jonathan.hess@gmail.com@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-01-14 20:58:02 +0000

    Fixed webapp deploy.  Now it works with Tar.  Implemented in the Izze project.

---
+++ jcatapult-ant/trunk/plugins/deploy/1.0/files/deploy.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+VERSION=$1
+ARCHIVE=$2
+
+PROJECT_NAME=@project.name@
+FILES_DIR=@deploy.dir.files@
+WORK_DIR=@deploy.dir.work@
+DEPLOY_DIR=@deploy.dir.deploy@
+STOP_CMD=@deploy.cmd.stop@
+START_CMD=@deploy.cmd.start@
+ENVIRONMENT=@deploy.environment@
+
+DB_HOST=@deploy.db.host@
+DB_USERNAME=@deploy.db.username@
+DB_PASSWORD=@deploy.db.password@
+DB_DATABASE=@deploy.db.database@
+
+
+NOW=`date +%Y-%m-%dT%H-%M-%S`
+
+echo Running $PROJECT_NAME Deploy Script on $ENVIRONMENT
+echo   Version: $VERSION
+echo   Archive: $ARCHIVE
+echo   Directories:
+echo     Files: $FILES_DIR
+echo     Work:  $WORK_DIR
+echo     Deploy:$DEPLOY_DIR
+echo   Commands:
+echo     Stop:  $STOP_CMD
+echo     Start: $START_CMD
+echo   Database:
+echo     Host:     $DB_HOST
+echo     Username: $DB_USERNAME
+echo     Password: $DB_PASSWORD
+echo     Database: $DB_DATABASE
+echo
+echo
+
+#BEGIN STEPS FOR TAR ONLY
+VERSION_DIR="$WORK_DIR/$VERSION"
+if [ -d "$VERSION_DIR" ] ; then
+	rm -rf $VERSION_DIR
+fi
+
+mkdir "$VERSION_DIR"
+
+#Untar the archive
+cd "$VERSION_DIR"
+if ! tar -zxf $WORK_DIR/$ARCHIVE ; then 
+	echo "Cannot open archive.  Exiting."
+	exit 1
+fi
+
+if [ ! -d web ] ; then 
+	echo "/web Directory missing.  Exiting."
+	exit 1
+fi
+
+
+#Copy web files for quick transition to production
+cp -r web web_stage
+
+
+#Stop the server
+echo Stopping Server...
+$STOP_CMD
+sleep 10
+
+#Redeploy Web Files
+echo Deploying Web Application Files
+mv $DEPLOY_DIR $VERSION_DIR/deploy_$NOW
+mv web_stage $DEPLOY_DIR
+
+#Load Database Scripts
+echo Loading Database Scripts TODO
+
+#Copy Files
+echo Copying Seed Files TODO
+
+#Start the Server
+echo Starting the server...
+$START_CMD
+
+if [ -d $WORK_DIR/CURRENT ] ; then
+	unlink $WORK_DIR/CURRENT
+fi
+
+ln -s $VERSION_DIR $WORK_DIR/CURRENT
+
+echo DONE!
