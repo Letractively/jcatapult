854d4f37fc97b921b617953567c961df62a8d515
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-02-04 21:00:27 +0000

    [JCATAPULT-ANT Tagging head] https://jcatapult.googlecode.com/svn/jcatapult-core/trunk to https://jcatapult.googlecode.com/svn/jcatapult-core/tags/1.0-M1

---
+++ jcatapult-core/tags/1.0-M1/src/java/main/org/jcatapult/domain/location/Address.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.jcatapult.domain.location;
+
+import java.io.Serializable;
+import javax.persistence.Embeddable;
+import javax.persistence.Column;
+
+/**
+ * <p>
+ * This class is a simple address object.
+ * </p>
+ *
+ * @author Brian Pontarelli
+ */
+@Embeddable
+public class Address implements Serializable {
+    private final static int serialVersionUID = 1;
+
+    @Column(nullable = false)
+    private String street1;
+
+    @Column(nullable = true)
+    private String street2;
+
+    @Column(nullable = true)
+    private String street3;
+
+    @Column(nullable = false)
+    private String city;
+
+    @Column(nullable = true)
+    private String state;
+
+    @Column(nullable = false)
+    private String country;
+
+    @Column(nullable = true, name = "postal_code")
+    private String postalCode;
+
+    public String getStreet1() {
+        return street1;
+    }
+
+    public void setStreet1(String street1) {
+        this.street1 = street1;
+    }
+
+    public String getStreet2() {
+        return street2;
+    }
+
+    public void setStreet2(String street2) {
+        this.street2 = street2;
+    }
+
+    public String getStreet3() {
+        return street3;
+    }
+
+    public void setStreet3(String street3) {
+        this.street3 = street3;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+}
