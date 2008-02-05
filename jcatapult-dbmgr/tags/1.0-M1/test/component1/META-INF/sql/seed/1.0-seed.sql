5ea9b38546e02d10c1e8accedba8e4c54bce2f59
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-02-05 03:53:44 +0000

    [JCATAPULT-ANT Tagging head] https://jcatapult.googlecode.com/svn/jcatapult-dbmgr/trunk to https://jcatapult.googlecode.com/svn/jcatapult-dbmgr/tags/1.0-M1

---
+++ jcatapult-dbmgr/tags/1.0-M1/test/component1/META-INF/sql/seed/1.0-seed.sql
@@ -0,0 +1,7 @@
+-- basic seeds into component1_table1
+insert into c1_t1 (id, name) values (1, '1.0-seed.sql-value1');
+insert into c1_t1 (id, name) values (2, '1.0-seed.sql-value2');
+
+-- this is a seed into a 1.0 alter table.  This insures that the 1.0 alter script
+-- ran before this script
+insert into c1_t2_p1_0 (id, c1_t1_id) values (1, 1);
