22c79551e42af979fce4e1521d788cc04b1fff94
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-03-26 22:25:35 +0000

    [JCATAPULT-ANT Tagging head] https://jcatapult.googlecode.com/svn/jcatapult-filemgr/trunk to https://jcatapult.googlecode.com/svn/jcatapult-filemgr/tags/1.0-M3

---
+++ jcatapult-filemgr/tags/1.0-M3/src/java/main/org/jcatapult/filemgr/action/FckFileManager.java
@@ -0,0 +1,40 @@
+package org.jcatapult.filemgr.action;
+
+import org.jcatapult.filemgr.domain.Connector;
+import org.jcatapult.filemgr.service.FileManagerService;
+import org.apache.struts2.convention.annotation.Results;
+import org.apache.struts2.convention.annotation.Result;
+
+import com.google.inject.Inject;
+
+/**
+ * <p>
+ * This class extends the {@link FileManager} action and provides the
+ * JavaScript response for file uploads.
+ * </p>
+ *
+ * @author Brian Pontarelli
+ */
+@Results({
+    @Result(name = "error", location = "", params = {"status", "500"}, type = "httpheader"),
+    @Result(name = "success", location = "", params = {"contentType", "text/xml", "inputName", "resultStream"}, type = "stream")
+})
+public class FckFileManager extends FileManager {
+    // Output for file upload from the service.
+    private Connector connector;
+
+    @Inject
+    public FckFileManager(FileManagerService fileManagerService) {
+        super(fileManagerService);
+    }
+
+    public Connector getConnector() {
+        return connector;
+    }
+
+    @Override
+    protected String doUpload() {
+        connector = fileManagerService.upload(getNewFile(), getNewFileFileName(), getNewFileContentType(), getType());
+        return SUCCESS;
+    }
+}
