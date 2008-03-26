91177d2f6150c497a5db443c160297f410ba8d37
leafknode <leafknode@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-03-26 21:32:55 +0000

    [JCATAPULT-ANT Tagging head] https://jcatapult.googlecode.com/svn/jcatapult-scaffolder/trunk to https://jcatapult.googlecode.com/svn/jcatapult-scaffolder/tags/1.0-M3

---
+++ jcatapult-scaffolder/tags/1.0-M3/src/java/main/org/jcatapult/scaffolder/lang/Type.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright 2007 (c) by Texture Media, Inc.
+ *
+ * This software is confidential and proprietary to
+ * Texture Media, Inc. It may not be reproduced,
+ * published or disclosed to others without company
+ * authorization.
+ */
+package org.jcatapult.scaffolder.lang;
+
+import javax.lang.model.type.TypeMirror;
+
+/**
+ * <p>
+ * This class models a type (Class, Interface, Enum).
+ * </p>
+ *
+ * @author Brian Pontarelli
+ */
+public abstract class Type extends Base {
+    protected final TypeMirror type;
+
+    public Type(TypeMirror type) {
+        this.type = type;
+    }
+
+    public abstract boolean isPrimitive();
+
+    public abstract boolean isSimpleType();
+}
