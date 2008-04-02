cd6fb20fa76a17101b22ec1a2ff93ee314a3c8db
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-04-02 15:29:56 +0000

    [JCATAPULT-ANT Tagging head] https://jcatapult.googlecode.com/svn/jcatapult-core/trunk to https://jcatapult.googlecode.com/svn/jcatapult-core/tags/1.0-M4

---
+++ jcatapult-core/tags/1.0-M4/src/conf/main/template/semantic/fieldheader.ftl
@@ -0,0 +1,51 @@
+<#include "/${parameters.templateDir}/semantic/dynamicAttributes.ftl"/>
+<#--
+	Only show message if errors are available.
+	This will be done if ActionSupport is used.
+-->
+<#assign hasFieldErrors = parameters.name?exists && fieldErrors?exists && fieldErrors[parameters.name]?exists/>
+
+<#--
+	if the label position is top, output the label in a div.
+-->
+<#if parameters.label?exists && parameters.labelposition?default("top") == 'top'>
+<div class="label" <#rt/>
+<#if parameters.id?exists>id="${parameters.id}-label"</#if><#rt/>>
+</#if>
+
+<#--
+  Handle the label
+-->
+<#if parameters.label?exists>
+<label <#t/>
+<#if parameters.id?exists>
+  for="${parameters.id?html}" <#t/>
+</#if>
+<#if hasFieldErrors>
+  class="error-label"<#t/>
+<#else>
+  class="label"<#t/>
+</#if>
+><#t/>
+<#if parameters.required?default(false) && parameters.requiredposition?default("right") != 'right'>
+<span class="required">*</span><#t/>
+</#if>
+${parameters.label?html}<#t/>
+<#if parameters.required?default(false) && parameters.requiredposition?default("right") == 'right'>
+<span class="required">*</span><#t/>
+</#if>
+<#if hasFieldErrors>
+  (${fieldErrors[parameters.name][0]?html})
+</#if>
+</label><#t/>
+</#if>
+<#lt/>
+<#if parameters.label?exists && parameters.labelposition?default("top") == 'top'>
+</div>
+</#if>
+<#if parameters.label?exists && parameters.labelposition?default("top") == 'top'>
+<div class="field" <#rt/>
+<#else>
+<span class="field" <#rt/>
+</#if>
+<#if parameters.id?exists>id="${parameters.id}-control"</#if><#rt/>>
---
+++ jcatapult-core/tags/1.0-M4/src/java/test/unit/org/jcatapult/action/StatesTest.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.jcatapult.action;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * <p>
+ * This class tests the states class.
+ * </p>
+ *
+ * @author  Brian Pontarelli and James Humphrey
+ */
+public class StatesTest {
+
+    @Test
+    public void testNonTerritoryStates() {
+        States stateAction = new States();
+        stateAction.execute();
+
+        ResourceBundle rb = ResourceBundle.getBundle("org.jcatapult.action.states");
+        Assert.assertEquals(rb.keySet().size(), stateAction.getStates().size());
+    }
+
+    @Test
+    public void testTerritoryStates() {
+        States stateAction = new States();
+        stateAction.setIncludeTerritories(true);
+        stateAction.execute();
+
+        ResourceBundle rb = ResourceBundle.getBundle("org.jcatapult.action.states-territories");
+        Assert.assertEquals(rb.keySet().size(), stateAction.getStates().size());
+    }
+
+    @Test
+    public void testBlankValue() {
+        States stateAction = new States();
+        stateAction.setIncludeBlank(true);
+        String blankValue = "-- SELECT A STATE --";
+        stateAction.setBlankValue(blankValue);
+        stateAction.execute();
+
+        ResourceBundle rb = ResourceBundle.getBundle("org.jcatapult.action.states");
+        Assert.assertEquals(rb.keySet().size() + 1, stateAction.getStates().size());
+
+        List<String> states = new ArrayList<String>(stateAction.getStates().keySet());
+
+        Assert.assertEquals(blankValue, stateAction.getStates().get(states.get(0)));
+    }
+
+    @Test
+    public void testBlankValueWithPreferred() {
+        States stateAction = new States();
+        stateAction.setIncludeBlank(true);
+        String blankValue = "-- SELECT A STATE --";
+        stateAction.setBlankValue(blankValue);
+        stateAction.setPreferredStates("CA");
+        stateAction.execute();
+
+        ResourceBundle rb = ResourceBundle.getBundle("org.jcatapult.action.states");
+        Assert.assertEquals(rb.keySet().size() + 1, stateAction.getStates().size());
+
+        List<String> states = new ArrayList<String>(stateAction.getStates().keySet());
+
+        Assert.assertEquals(blankValue, stateAction.getStates().get(states.get(0)));
+        Assert.assertEquals("California", stateAction.getStates().get(states.get(1)));
+    }
+}
