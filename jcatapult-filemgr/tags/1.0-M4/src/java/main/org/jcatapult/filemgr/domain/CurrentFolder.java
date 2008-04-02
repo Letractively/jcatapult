099148786edd543cc17a015e69d5f37e8dbbd057
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-04-02 16:25:55 +0000

    [JCATAPULT-ANT Tagging head] https://jcatapult.googlecode.com/svn/jcatapult-filemgr/trunk to https://jcatapult.googlecode.com/svn/jcatapult-filemgr/tags/1.0-M4

---
+++ jcatapult-filemgr/tags/1.0-M4/src/java/main/org/jcatapult/filemgr/domain/CurrentFolder.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright (c) 2001-2007, Inversoft, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.jcatapult.filemgr.domain;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+
+/**
+ * <p>
+ * This class represents the current folder that the File Manager is
+ * interacting with and is passed back to the client from the
+ * server in order to specify the URL that the File Manager should use
+ * for this folder.
+ * </p>
+ *
+ * @author  Brian Pontarelli
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+public class CurrentFolder {
+    @XmlAttribute()
+    private String path;
+
+    @XmlAttribute()
+    private String url;
+
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        if (url != null && !url.endsWith("/")) {
+            url += "/";
+        }
+
+        this.url = url;
+    }
+}
