82e3849a27ff52be400b7b86de29b8b2de73d83f
leafknode <leafknode@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-04-03 17:44:49 +0000

    [JCATAPULT-ANT Tagging head] https://jcatapult.googlecode.com/svn/jcatapult-scaffolder/trunk to https://jcatapult.googlecode.com/svn/jcatapult-scaffolder/tags/1.0-M4

---
+++ jcatapult-scaffolder/tags/1.0-M4/src/scaffolder/main/crud/ftls/add.ftl
@@ -0,0 +1,8 @@
+${r"[#ftl/]"}
+<html>
+  <head><title>${type.name} | Add</title></head>
+  <body>
+    [#global actionType="save"/]
+    [#include "form.ftl"/]
+  </body>
+</html>
---
+++ jcatapult-scaffolder/tags/1.0-M4/src/script/main/scaffold.bat
@@ -0,0 +1,46 @@
+@echo off
+
+::
+:: Run ant jar first
+::
+echo Running ant build
+call ant jar> output
+del output
+
+::
+:: Build up the scaffolding classpath
+::
+set DIR=%~dp0
+
+::
+:: Build the scaffolders classpath
+::
+setlocal EnableDelayedExpansion
+set JarsDir=D:\ant\apache-ant-1.6.1\lib
+set cp=
+for %%f in (%DIR%..\lib\*.jar) do set cp=!cp!;%%f
+endlocal & set RUN_CLASSPATH=%cp%
+:: echo Scaffolders classpath is %RUN_CLASSPATH%
+
+::
+:: Grab the project JARs
+::
+setlocal EnableDelayedExpansion
+set JarsDir=D:\ant\apache-ant-1.6.1\lib
+set cp=
+for %%f in (target\jars\*.jar) do set cp=!cp!;%%f
+endlocal & set LOCAL_CLASSPATH=%cp%
+:: echo Local classpath is %LOCAL_CLASSPATH%
+
+::
+:: Grab the deps and transitive deps for the project
+::
+echo Using Savant to determine project classpath
+set PROJECT_CLASSPATH=
+FOR /F "delims==" %%G IN ('java -cp "%RUN_CLASSPATH%" net.java.savant.dep.DependencyPathMain') DO set PROJECT_CLASSPATH=%%G
+:: echo Savant classpath is %PROJECT_CLASSPATH%
+
+::
+:: Execute the scaffolder
+::
+java -cp "%JAVA_HOME%\lib\tools.jar;%RUN_CLASSPATH%;%LOCAL_CLASSPATH%;%PROJECT_CLASSPATH%" -Dscaffolder.home=%DIR%\.. org.jcatapult.scaffolder.Main %1
