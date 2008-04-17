1398ace5b25b991a6067b769486a086477e035c2
leafknode <leafknode@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-04-17 04:57:13 +0000

    [JCATAPULT-ANT Tagging head] https://jcatapult.googlecode.com/svn/jcatapult-filemgr/trunk to https://jcatapult.googlecode.com/svn/jcatapult-filemgr/tags/1.0-M5

---
+++ jcatapult-filemgr/tags/1.0-M5/src/java/test/unit/org/jcatapult/filemgr/BaseTest.java
@@ -0,0 +1,12 @@
+package org.jcatapult.filemgr;
+
+import org.junit.Ignore;
+
+/**
+ * User: jhumphrey
+ * Date: Mar 28, 2008
+ */
+@Ignore
+public class BaseTest {
+    protected final String testDir = System.getProperty("user.home") + "/.jcatapult/testing-filemgr";
+}
---
+++ jcatapult-filemgr/tags/1.0-M5/src/java/test/unit/org/jcatapult/filemgr/servlet/FileServletTest.java
@@ -0,0 +1,130 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.jcatapult.filemgr.servlet;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.configuration.Configuration;
+import org.easymock.EasyMock;
+import org.jcatapult.guice.GuiceContainer;
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.google.inject.Binder;
+import com.google.inject.Module;
+
+/**
+ * <p>
+ * This class tests the file servlet.
+ * </p>
+ *
+ * @author Brian Pontarelli
+ */
+public class FileServletTest {
+    @Test
+    public void testNoInjector() {
+        FileServlet fs = new FileServlet();
+        try {
+            fs.init();
+            Assert.fail("Should have failed because of no injector");
+        } catch (ServletException e) {
+        }
+
+        GuiceContainer.setGuiceModules(new Module(){
+            public void configure(Binder binder) {
+            }
+        });
+        GuiceContainer.initialize();
+        try {
+            fs.init();
+            Assert.fail("Should have failed because of no Configuration");
+        } catch (ServletException e) {
+        }
+    }
+
+    @Test
+    public void testMissingFile() throws ServletException, IOException {
+        final Configuration configuration = EasyMock.createStrictMock(Configuration.class);
+        EasyMock.expect(configuration.getString("jcatapult.file-mgr.file-servlet.dir", System.getProperty("user.home") + "/data")).
+            andReturn("src/java/test/unit/org/jcatapult/filemgr/servlet");
+        EasyMock.replay(configuration);
+
+        GuiceContainer.setGuiceModules(new Module() {
+            public void configure(Binder binder) {
+                binder.bind(Configuration.class).toInstance(configuration);
+            }
+        });
+        GuiceContainer.initialize();
+
+        FileServlet fs = new FileServlet();
+        fs.init();
+
+        HttpServletRequest request = EasyMock.createStrictMock(HttpServletRequest.class);
+        EasyMock.expect(request.getServletPath()).andReturn("/files");
+        EasyMock.expect(request.getRequestURI()).andReturn("/missing-file.xml");
+        EasyMock.replay(request);
+
+        HttpServletResponse response = EasyMock.createStrictMock(HttpServletResponse.class);
+        response.setStatus(404);
+        EasyMock.replay(response);
+
+        fs.doGet(request, response);
+        EasyMock.verify(request, response);
+    }
+
+    @Test
+    public void testSuccess() throws ServletException, IOException {
+        final Configuration configuration = EasyMock.createStrictMock(Configuration.class);
+        EasyMock.expect(configuration.getString("jcatapult.file-mgr.file-servlet.dir", System.getProperty("user.home") + "/data")).
+            andReturn("src/java/test/unit/org/jcatapult/filemgr/servlet");
+        EasyMock.replay(configuration);
+
+        GuiceContainer.setGuiceModules(new Module(){
+            public void configure(Binder binder) {
+                binder.bind(Configuration.class).toInstance(configuration);
+            }
+        });
+        GuiceContainer.initialize();
+
+        FileServlet fs = new FileServlet();
+        fs.init();
+
+        HttpServletRequest request = EasyMock.createStrictMock(HttpServletRequest.class);
+        EasyMock.expect(request.getServletPath()).andReturn("/files");
+        EasyMock.expect(request.getRequestURI()).andReturn("/test-file.xml");
+        EasyMock.replay(request);
+
+        final StringBuilder build = new StringBuilder();
+        ServletOutputStream sos = new ServletOutputStream() {
+            public void write(int b) throws IOException {
+                build.append(new String(new int[]{b}, 0, 1));
+            }
+        };
+
+        HttpServletResponse response = EasyMock.createStrictMock(HttpServletResponse.class);
+        EasyMock.expect(response.getOutputStream()).andReturn(sos);
+        EasyMock.replay(response);
+
+        fs.doGet(request, response);
+
+        Assert.assertEquals("hello", build.toString());
+        EasyMock.verify(request, response);
+    }
+}
