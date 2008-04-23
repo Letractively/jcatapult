c07e4a3d9d9ccf28e4f91e3eb07fda60654c7756
leafknode <leafknode@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-04-23 20:16:56 +0000

    [JCATAPULT-ANT Tagging head] https://jcatapult.googlecode.com/svn/jcatapult-scaffolder/trunk to https://jcatapult.googlecode.com/svn/jcatapult-scaffolder/tags/1.0-M6

---
+++ jcatapult-scaffolder/tags/1.0-M6/src/java/test/unit/org/jcatapult/scaffolder/Inner.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright 2007 (c) by Texture Media, Inc.
+ *
+ * This software is confidential and proprietary to
+ * Texture Media, Inc. It may not be reproduced,
+ * published or disclosed to others without company
+ * authorization.
+ */
+package org.jcatapult.scaffolder;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+
+/**
+ * <p>
+ * This is a test embeddable object.
+ * </p>
+ *
+ * @author  Brian Pontarelli
+ */
+@Embeddable
+public class Inner {
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+    @Column(name = "last_name")
+    private String lastName;
+    private InnerInner inner;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public InnerInner getInner() {
+        return inner;
+    }
+
+    public void setInner(InnerInner inner) {
+        this.inner = inner;
+    }
+}
