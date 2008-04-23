45912df56dba659d73cd76d7097867b193a2d1e0
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-04-23 00:19:08 +0000

    Got the first pass at the transaction management stuff

---
+++ jcatapult-core/trunk/src/java/test/unit/org/jcatapult/persistence/TransactionTest.java
@@ -0,0 +1,120 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ */
+package org.jcatapult.persistence;
+
+import java.sql.SQLException;
+import javax.sql.RowSet;
+
+import org.jcatapult.jpa.User;
+import org.jcatapult.persistence.annotation.Transactional;
+import org.jcatapult.test.JPABaseTest;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+import com.google.inject.Inject;
+
+/**
+ * <p>
+ * This class tests the transactin annotation and the defaults at
+ * the macro and micro levels.
+ * </p>
+ *
+ * @author  Brian Pontarelli
+ */
+public class TransactionTest extends JPABaseTest {
+    private JPATestService service;
+
+    @Inject
+    public void setService(JPATestService service) {
+        this.service = service;
+    }
+
+    @Test
+    public void testMarco() throws SQLException {
+        service.success();
+        RowSet rs = executeQuery("select name from User where name = 'TransactionTest-success'");
+        assertTrue(rs.next());
+        rs.close();
+
+        try {
+            service.failure();
+        } catch (Exception e) {
+            // Expected
+        }
+        rs = executeQuery("select name from User where name = 'TransactionTest-failure'");
+        assertFalse(rs.next());
+        rs.close();
+
+        service.returnValueSuccess();
+        rs = executeQuery("select name from User where name = 'TransactionTest-returnValueSuccess'");
+        assertTrue(rs.next());
+        rs.close();
+
+        service.returnValueFailure();
+        rs = executeQuery("select name from User where name = 'TransactionTest-returnValueFailure'");
+        assertFalse(rs.next());
+        rs.close();
+    }
+
+    public static class JPATestService {
+        private final PersistenceService persistenceService;
+
+        @Inject
+        public JPATestService(PersistenceService persistenceService) {
+            this.persistenceService = persistenceService;
+        }
+
+        @Transactional()
+        public void success() {
+            User user = new User();
+            user.setName("TransactionTest-success");
+            persistenceService.persist(user);
+        }
+
+        @Transactional()
+        public void failure() {
+            User user = new User();
+            user.setName("TransactionTest-failure");
+            persistenceService.persist(user);
+            throw new RuntimeException();
+        }
+
+        @Transactional(processor = UserProcessor.class)
+        public User returnValueSuccess() {
+            User user = new User();
+            user.setName("TransactionTest-returnValueSuccess");
+            persistenceService.persist(user);
+            return user;
+        }
+
+        @Transactional(processor = UserProcessor.class)
+        public User returnValueFailure() {
+            User user = new User();
+            user.setName("TransactionTest-returnValueFailure");
+            persistenceService.persist(user);
+            return user;
+        }
+    }
+
+    public static class UserProcessor extends DefaultTransactionResultProcessor {
+        @Override
+        public boolean rollback(Object result, Throwable throwable) {
+            return ((User) result).getName().equals("TransactionTest-returnValueFailure") ||
+                super.rollback(result, throwable);
+        }
+    }
+}
