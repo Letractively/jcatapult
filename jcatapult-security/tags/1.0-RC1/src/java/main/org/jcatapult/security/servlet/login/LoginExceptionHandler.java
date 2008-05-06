ecb58c9ec5fb2788ed01d03d0ad246424f9b1be7
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-05-06 22:20:15 +0000

    [JCATAPULT-ANT Tagging head] https://jcatapult.googlecode.com/svn/jcatapult-security/trunk to https://jcatapult.googlecode.com/svn/jcatapult-security/tags/1.0-RC1

---
+++ jcatapult-security/tags/1.0-RC1/src/java/main/org/jcatapult/security/servlet/login/LoginExceptionHandler.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.jcatapult.security.servlet.login;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+import org.jcatapult.security.JCatapultSecurityException;
+import org.jcatapult.servlet.WorkflowChain;
+
+import com.google.inject.ImplementedBy;
+
+/**
+ * <p>
+ * This interface is used to handle all exceptions that are thrown when
+ * a login fails. The only two exceptions that implements should be
+ * concerned with are {@link org.jcatapult.security.login.InvalidUsernameException}
+ * and {@link org.jcatapult.security.login.InvalidPasswordException}.
+ * </p>
+ *
+ * @author Brian Pontarelli
+ */
+@ImplementedBy(DefaultLoginExceptionHandler.class)
+public interface LoginExceptionHandler {
+    /**
+     * Handles the exception.
+     *
+     * @param   exception The exception that was thrown.
+     * @param   request The request for forwarding and including.
+     * @param   response The response for redirects, forwarding and including.
+     * @param   workflowChain The workflow chain in case the implementation wants to keep going down
+     *          the chain.
+     * @throws  ServletException If something goes wrong during the exception handling.
+     * @throws  IOException If something goes wrong during the exception handling.
+     */
+    void handle(JCatapultSecurityException exception, ServletRequest request, ServletResponse response,
+            WorkflowChain workflowChain)
+    throws ServletException, IOException;
+}
