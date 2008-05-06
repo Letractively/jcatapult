9eba9f74b5e376952734a140a4792afa584f8b70
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-05-06 20:38:23 +0000

    [JCATAPULT-ANT Tagging head] https://jcatapult.googlecode.com/svn/jcatapult-core/trunk to https://jcatapult.googlecode.com/svn/jcatapult-core/tags/1.0-RC1

---
+++ jcatapult-core/tags/1.0-RC1/src/java/main/org/jcatapult/guice/StrutsModule.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ */
+package org.jcatapult.guice;
+
+import org.jcatapult.servlet.ServletObjectsHolder;
+import org.jcatapult.struts.action.annotation.ActionName;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Provider;
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.DefaultActionInvocation;
+
+/**
+ * <p>
+ * This is the a module that sets up Struts injection points. Currently,
+ * this only provides the ability to inject the current action name into
+ * classes.
+ * </p>
+ *
+ * @author  Brian Pontarelli
+ */
+public class StrutsModule extends AbstractModule {
+    /**
+     * Calls these methods in this order:
+     *
+     * <ol>
+     * <li>{@link #configureActionName()}</li>
+     * </ol>
+     */
+    @Override
+    protected void configure() {
+        if (ServletObjectsHolder.getServletContext() == null) {
+            return;
+        }
+
+        configureActionName();
+    }
+
+    /**
+     * Configures the action name.
+     */
+    protected void configureActionName() {
+        // Bind the servlet context
+        bind(String.class).annotatedWith(ActionName.class).toProvider(new Provider<String>() {
+            public String get() {
+                DefaultActionInvocation invocation = (DefaultActionInvocation) ActionContext.getContext().getActionInvocation();
+                String namespace = invocation.getProxy().getNamespace();
+                String actionName = invocation.getProxy().getActionName();
+                if (actionName == null) {
+                    actionName = "index"; // Struts2 convention that empty action names are index
+                }
+                return namespace + "/" + actionName;
+            }
+        });
+    }
+}
---
+++ jcatapult-core/tags/1.0-RC1/src/java/test/unit/org/jcatapult/test/ModuleBaseTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ */
+package org.jcatapult.test;
+
+import org.junit.Ignore;
+
+/**
+ * <p>
+ * This class can be used for testing module classes.
+ * </p>
+ *
+ * @author  Brian Pontarelli
+ */
+@Ignore
+public abstract class ModuleBaseTest extends WebBaseTest {
+
+    /**
+     * Returns the directory of the web root for a module which is currently the
+     * <code>src/web/test</code> directory. This is done since the <code>src/web/main</code>
+     * directory really only contains FreeMarker templates and the test directory might contain
+     * configuration files used for testing.
+     *
+     * @return  The module web root directory.
+     */
+    protected String getWebRootDir() {
+        return "src/web/test";
+    }
+}
