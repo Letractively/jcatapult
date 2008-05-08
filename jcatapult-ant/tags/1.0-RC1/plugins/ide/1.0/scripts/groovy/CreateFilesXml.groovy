47eb0f641dfe2f9c2820cb98441d144a2bb32206
leafknode <leafknode@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-05-08 15:02:35 +0000

    tagging 1.0-RC1

---
+++ jcatapult-ant/tags/1.0-RC1/plugins/ide/1.0/scripts/groovy/CreateFilesXml.groovy
@@ -0,0 +1,36 @@
+/**
+ * Unfortunately, I want the transitive dependencies and there isn't an easy way yet from ant
+ * to get them without writing some code. So, I create a path structure and then parse it to
+ * create some XML that I can then transform.
+ *
+ * Parses the 'path' variable and creates an xml file from all the dependency paths in the following format:
+ *
+ * <files>
+ *   <file path="PATH TO DEPENDENCY IN THE SAVANT REPOS" src="PATH TO SOURCE IN THE SAVANT REPOS"/>
+ * </files>
+ *
+ * User: jhumphrey
+ * Date: Oct 22, 2007
+ */
+
+import net.java.io.FileTools;
+
+String[] libs = properties["path"].split(/,/);
+StringBuffer buf = new StringBuffer();
+buf.append("<files>");
+for (String lib in libs) {
+  lib = lib.replace("\\", "/");
+  String home = System.getProperty("user.home").replace("\\", "/");
+  String path = lib.replace(home + "/.savant_repository/", "");
+  String src = path.replace(".jar", "-src.jar");
+  int index = path.lastIndexOf("/");
+  String name = path.substring(index + 1);
+
+  buf.append("<file path=\"").append(path).append("\" src=\"").append(src).append("\"");
+  buf.append(" name=\"").append(name).append("\"/>");
+}
+buf.append("</files>");
+
+File file = new File(System.getProperty("java.io.tmpdir") + "/files.xml");
+file.delete();
+FileTools.write(file, buf.toString());
