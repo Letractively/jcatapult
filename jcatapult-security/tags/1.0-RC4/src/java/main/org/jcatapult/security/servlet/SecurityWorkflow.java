a606f29336ca3cb274d1d7cbfbdb66174826db30
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-06-11 23:03:04 +0000

    [JCATAPULT-ANT Tagging head] https://jcatapult.googlecode.com/svn/jcatapult-security/trunk to https://jcatapult.googlecode.com/svn/jcatapult-security/tags/1.0-RC4

---
+++ jcatapult-security/tags/1.0-RC4/src/java/main/org/jcatapult/security/servlet/SecurityWorkflow.java
@@ -0,0 +1,89 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.jcatapult.security.servlet;
+
+import java.io.IOException;
+import java.util.Arrays;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jcatapult.security.servlet.auth.AuthorizationWorkflow;
+import org.jcatapult.security.servlet.login.LoginWorkflow;
+import org.jcatapult.security.servlet.saved.SavedRequestWorkflow;
+import org.jcatapult.servlet.SubWorkflowChain;
+import org.jcatapult.servlet.Workflow;
+import org.jcatapult.servlet.WorkflowChain;
+
+import com.google.inject.Inject;
+
+/**
+ * <p>
+ * This class is the main security workflow that attaches to the JCatapult
+ * Workflow system and provides the functionality for handling all the JCatapult
+ * security needs. In fact, this is a sub-workflow. It uses a number of other
+ * Workflow implementations in a specific order. Here is the default ordering
+ * of the Workflows this class uses.
+ * </p>
+ *
+ * <p>
+ * This class uses {@link SubWorkflowChain} to handle the call to all of
+ * the workflows in the constructor. These are called in this order:
+ * </p>
+ *
+ * <ol>
+ * <li>{@link CredentialStorageWorkflow}</li>
+ * <li>{@link LoginWorkflow}</li>
+ * <li>{@link AuthorizationWorkflow}</li>
+ * </ol>
+ *
+ * @author  Brian Pontarelli
+ */
+public class SecurityWorkflow implements Workflow {
+    private final CredentialStorageWorkflow credentialStorageWorkflow;
+    private final SavedRequestWorkflow savedRequestWorkflow;
+    //    private final RememberMeWorkflow rememberMeWorkflow;
+    private final LoginWorkflow loginWorkflow;
+    private final AuthorizationWorkflow authorizationWorkflow;
+
+    @Inject
+    public SecurityWorkflow(CredentialStorageWorkflow credentialStorageWorkflow, SavedRequestWorkflow savedRequestWorkflow,
+            LoginWorkflow loginWorkflow, AuthorizationWorkflow authorizationWorkflow) {
+        this.credentialStorageWorkflow = credentialStorageWorkflow;
+        this.savedRequestWorkflow = savedRequestWorkflow;
+        this.loginWorkflow = loginWorkflow;
+        this.authorizationWorkflow = authorizationWorkflow;
+    }
+
+    /**
+     * Creates a sub-workflow chain that calls the sub-workflows in the order from the class comment.
+     *
+     * @param   request The request which is passed to the sub-workflow chain.
+     * @param   response The response which is passed to the sub-workflow chain.
+     * @param   chain The workflow chain, which is the end point of the sub-workflow chain.
+     * @throws  IOException If the chain throws.
+     * @throws  ServletException If the chain throws.
+     */
+    public void perform(HttpServletRequest request, HttpServletResponse response, WorkflowChain chain)
+    throws IOException, ServletException {
+        SubWorkflowChain subChain = new SubWorkflowChain(Arrays.asList(credentialStorageWorkflow, savedRequestWorkflow,
+            loginWorkflow, authorizationWorkflow), chain);
+        subChain.doWorkflow(request, response);
+    }
+
+    public void destroy() {
+    }
+}
---
+++ jcatapult-security/tags/1.0-RC4/src/java/main/org/jcatapult/security/servlet/auth/AuthorizationWorkflow.java
@@ -0,0 +1,112 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.jcatapult.security.servlet.auth;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jcatapult.security.EnhancedSecurityContext;
+import org.jcatapult.security.auth.AuthorizationException;
+import org.jcatapult.security.auth.Authorizer;
+import org.jcatapult.security.auth.NotLoggedInException;
+import org.jcatapult.servlet.Workflow;
+import org.jcatapult.servlet.WorkflowChain;
+
+import com.google.inject.Inject;
+
+/**
+ * <p>
+ * This class is a workflow that grabs the incoming request URI and
+ * authorizes the current user (or current request if there is no user)
+ * against that request URI.
+ * </p>
+ *
+ * <p>
+ * If the authorization fails, this class does one of two things based
+ * on the type of failure.
+ * </p>
+ *
+ * <h3>Not logged it</h3>
+ * <p>
+ * If the user is not logged in and needs to be, this class delegates
+ * control to an implementation of the {@link NotLoggedInHandler}.
+ * </p>
+ *
+ * <h3>Invalid permissions</h3>
+ * <p>
+ * If the user is logged in but doesn't have the correct permissions for
+ * the request URI, this class delegates control to an implementation of
+ * the {@link AuthorizationExceptionHandler}
+ * </p>
+ *
+ * <p>
+ * Whenever a failure occurs as described above, the workflow is not
+ * executed so that those classes can take control. Otherwise, the workflow
+ * chain is executed to continue processing the request.
+ * </p>
+ *
+ * @author Brian Pontarelli
+ */
+public class AuthorizationWorkflow implements Workflow {
+    private final Authorizer authorizer;
+    private final NotLoggedInHandler notLoggedInHandler;
+    private final AuthorizationExceptionHandler authorizationExceptionHandler;
+
+    @Inject
+    public AuthorizationWorkflow(Authorizer authorizer, NotLoggedInHandler notLoggedInHandler,
+            AuthorizationExceptionHandler authorizationExceptionHandler) {
+        this.authorizer = authorizer;
+        this.notLoggedInHandler = notLoggedInHandler;
+        this.authorizationExceptionHandler = authorizationExceptionHandler;
+    }
+
+    /**
+     * Grabs the current request URI and then sends it along with the current user from the
+     * {@link org.jcatapult.security.EnhancedSecurityContext} to the {@link Authorizer} to be
+     * authorized. If authorization fails, this delegates control to the
+     * {@link AuthorizationExceptionHandler}. If there is not a logged in user and there must
+     * be one to verify credentials (i.e. the authorizer threw a NotLoggedInException), this
+     * delegates control to the {@link NotLoggedInHandler}.
+     *
+     * @param   request The HTTP request to get the request URI from.
+     * @param   response Not used.
+     * @param   workflowChain The workflow chain which is called if the authorization passes.
+     * @throws  IOException If the chain throws.
+     * @throws  ServletException If the chain throws.
+     */
+    public void perform(HttpServletRequest request, HttpServletResponse response, WorkflowChain workflowChain)
+    throws IOException, ServletException {
+        String uri = request.getRequestURI();
+        Object user = EnhancedSecurityContext.getCurrentUser();
+
+        try {
+            authorizer.authorize(user, uri);
+        } catch (AuthorizationException e) {
+            authorizationExceptionHandler.handle(e, request, response, workflowChain);
+            return;
+        } catch (NotLoggedInException e) {
+            notLoggedInHandler.handle(e, request, response, workflowChain);
+            return;
+        }
+
+        workflowChain.doWorkflow(request, response);
+    }
+
+    public void destroy() {
+    }
+}
