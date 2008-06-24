e5fd56138637057f9bc4580dc2069851561d0fd9
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-06-24 14:30:23 +0000

    Finished prepareAction on form

--- jcatapult-mvc/trunk/src/java/test/unit/org/jcatapult/mvc/result/control/FormTest.java
+++ jcatapult-mvc/trunk/src/java/test/unit/org/jcatapult/mvc/result/control/FormTest.java
@@ -15,15 +15,22 @@
  */
 package org.jcatapult.mvc.result.control;
 
+import java.io.IOException;
 import java.io.StringWriter;
 import java.util.HashMap;
 import java.util.Locale;
+import java.util.concurrent.atomic.AtomicBoolean;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
 
 import org.easymock.EasyMock;
 import org.jcatapult.container.ContainerResolver;
 import org.jcatapult.freemarker.DefaultFreeMarkerService;
 import org.jcatapult.freemarker.FreeMarkerService;
 import org.jcatapult.freemarker.OverridingTemplateLoader;
+import org.jcatapult.mvc.servlet.MVCWorkflow;
+import org.jcatapult.servlet.ServletObjectsHolder;
+import org.jcatapult.servlet.WorkflowChain;
 import static org.junit.Assert.*;
 import org.junit.Test;
 
@@ -39,7 +46,7 @@ import static net.java.util.CollectionTools.*;
 public class FormTest extends AbstractControlTest {
     @Test
     public void testNoPrepare() {
-        Form form = new Form();
+        Form form = new Form(null);
         FreeMarkerService fms = new DefaultFreeMarkerService(makeConfiguration(), makeEnvironmenResolver(),
             new OverridingTemplateLoader(makeContainerResolver()));
         form.setServices(Locale.US, makeRequest(), makeActionInvocationStore(null, "/test"),
@@ -51,6 +58,38 @@ public class FormTest extends AbstractControlTest {
             "<div class=\"form\">\n" +
             "<form action=\"/test\" method=\"POST\"></form>\n" +
             "</div>", writer.toString());
+    }
+
+    @Test
+    public void testPrepare() throws IOException, ServletException {
+        final AtomicBoolean called = new AtomicBoolean(false);
+        MVCWorkflow workflow = new MVCWorkflow() {
+            public void perform(WorkflowChain workflowChain) {
+                called.set(true);
+                assertEquals("/prepare", ServletObjectsHolder.getServletRequest().getRequestURI());
+            }
+        };
+
+        HttpServletRequest request = EasyMock.createStrictMock(HttpServletRequest.class);
+        EasyMock.expect(request.getRequestURI()).andReturn("/test");
+        EasyMock.replay(request);
+
+        Form form = new Form(workflow);
+        FreeMarkerService fms = new DefaultFreeMarkerService(makeConfiguration(), makeEnvironmenResolver(),
+            new OverridingTemplateLoader(makeContainerResolver()));
+        form.setServices(Locale.US, request, makeActionInvocationStore(null, "/test"),
+            makeMessageStore(null), fms);
+        StringWriter writer = new StringWriter();
+        form.renderStart(writer, mapNV("action", "/test", "method", "POST", "prepareAction", "/prepare"), new HashMap<String, String>());
+        form.renderEnd(writer);
+        assertEquals(
+            "<div class=\"form\">\n" +
+            "<form action=\"/test\" method=\"POST\"></form>\n" +
+            "</div>", writer.toString());
+        assertTrue(called.get());
+        assertEquals("/test", ServletObjectsHolder.getServletRequest().getRequestURI());
+
+        EasyMock.verify(request);
     }
 
     protected ContainerResolver makeContainerResolver() {
