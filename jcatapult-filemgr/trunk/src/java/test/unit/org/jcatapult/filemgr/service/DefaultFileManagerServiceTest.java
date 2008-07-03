1f75abd51aab9ea6926c8227bce4b5bed7f96ea3
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-07-03 17:38:12 +0000

    Updated to new configuration interface and organized imports

--- jcatapult-filemgr/trunk/src/java/test/unit/org/jcatapult/filemgr/service/DefaultFileManagerServiceTest.java
+++ jcatapult-filemgr/trunk/src/java/test/unit/org/jcatapult/filemgr/service/DefaultFileManagerServiceTest.java
@@ -21,8 +21,8 @@ import java.util.List;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
-import org.apache.commons.configuration.Configuration;
 import org.easymock.EasyMock;
+import org.jcatapult.config.Configuration;
 import org.jcatapult.filemgr.BaseTest;
 import org.jcatapult.filemgr.domain.Connector;
 import org.jcatapult.filemgr.domain.Folder;
