10ccdf29a997d2fdb561590af7000be5d4952e48
leafknode <leafknode@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-08-08 21:42:21 +0000

    branching jdk5 version

---
+++ jcatapult-struts/branches/1.0-RC6-jdk5/src/java/main/org/jcatapult/domain/commerce/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * <p>
+ * This package contains the JCatapult commerce integration points for Struts2
+ * in order to support Money values being submitted from forms and also
+ * conversion to Strings so that Money can be print out on a JSP or FTL.
+ * </p>
+ */
+package org.jcatapult.domain.commerce;
