6c733ff6e3254ec4c7eb9d00489d45598ce68f81
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-09-17 00:19:14 +0000

    Updates to release and scripts to better handle projects and DBMgr release

--- jcatapult-assets/trunk/scripts/release.sh
+++ jcatapult-assets/trunk/scripts/release.sh
@@ -6,52 +6,108 @@ if [ $# != 1 ]; then
 fi
 
 version=$1
-projects="jcatapult-core jcatapult-ant jcatapult-commerce jcatapult-dbmgr jcatapult-deployer jcatapult-email jcatapult-mvc jcatapult-persistence jcatapult-scaffolder jcatapult-security jcatapult-struts jcatapult-filemgr"
+dir=$(dirname $0)
+projects=$(cat $dir/projects)
 
-# Run some checks
-for p in $projects; do
-  if [ ! -d $p ]; then
-    echo "You need to check out the $p project."
+#
+# This runs all the necessary checks against the projects
+#
+function test_project() {
+  if [ ! -d $1 ]; then
+    echo "You need to check out the $1 project."
     exit 1
   fi
 
-  cd $p
+  cd $1
 
   changes=$(svn stat | wc -l)
   if (( $changes > 0 )); then
-    echo "$p has pending changes"
+    echo "$1 has pending changes"
     exit 1
   fi
 
-  if ! ant clean test; then
+  if ! ant clean test > /dev/null; then
     echo "Project tests failed. Unable to release"
     exit 1
   fi
 
   cd ..
+}
+
+#
+# This updates the version in the project
+#
+function update_version() {
+  cd $1
+
+  sed "s/<property name=\"version.jcatapult\" value=\".*\"\/>/<property name=\"version.jcatapult\" value=\"${version}\"\/>/g" project.xml | \
+    sed "s/version=\".*\">/version=\"${version}\">/g" > tmp-project.xml
+  rm project.xml
+  mv tmp-project.xml project.xml
+
+  if ! svn ci -m "Next version"; then
+    echo "Unable to commit modified project.xml"
+    exit 1
+  fi
+
+  cd ..
+}
+
+#
+# This releases the project
+#
+function release_project() {
+  cd $1
+
+  if ! ant rel < $dir/release-input > /dev/null; then
+    echo "Release of $1 failed"
+    exit 1
+  fi
+
+  if [ $1 = "jcatapult-dbmgr" ]; then
+    echo "Here"
+
+    # We need to grab the release of the jcatapult-dbmgr and copy it to jcat-ant
+    if ! svn rm ../jcatapult-ant/plugins/mysql/1.0/lib/jcatapult*; then
+      echo "Unable to remove previous jcatapult-dbmgr from jcatapult-ant MySQL plugin"
+      exit 1
+    fi
+
+    if ! cp target/jars/jcatapult-dbmgr-${version}.jar ../jcatapult-ant/plugins/mysql/1.0/lib/; then
+      echo "Unable to update jcatapult-ant MySQL plugin with the new jcatpault-dbmgr"
+      exit 1
+    fi
+
+    if ! svn add ../jcatapult-ant/plugins/mysql/1.0/lib/jcatapult*; then
+      echo "Unable to add new jcatapult-dbmgr to the jcatapult-ant MySQL plugin"
+      exit 1
+    fi
+
+    if ! svn ci -m "Next version" ../jcatapult-ant; then
+      echo "Unable to commit new jcatapult-dbmgr to the jcatapult-ant MySQL plugin"
+      exit 1
+    fi
+  fi
+
+  cd ..
+}
+
+# Run some checks
+for p in $projects; do
+  echo "Testing $p"
+  test_project $p
 done
 
 # Fix the project.xml files
 for p in $projects; do
-  if [ -f $p/project.xml ]; then
-    cd $p
-    sed "s/<property name=\"version.jcatapult\" value=\".*\"\/>/<property name=\"version.jcatapult\" value=\"${version}\"\/>/g" project.xml | \
-      sed "s/version=\".*\">/version=\"${version}\">/g" > tmp-project.xml
-    rm project.xml
-    mv tmp-project.xml project.xml
+  echo "Updating $p"
+  update_version $p
+done
 
-    if ! svn ci -m "Next version"; then
-      echo "Unable to commit modified project.xml"
-      exit 1
-    fi
-
-    if ! ant rel < ../release-input; then
-      echo "Release of $p failed"
-      exit 1
-    fi
-
-    cd ..
-  fi
+# First release the DBMgr and then copy that release to jcatapult ant
+for p in $projects; do
+  echo "Releasing $p"
+  release_project $p
 done
 
 # Push the ZIP file to the site
