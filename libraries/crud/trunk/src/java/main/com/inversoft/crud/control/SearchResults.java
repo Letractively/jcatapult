606268f75a1c0f385bbaf43a397bb23dff4b4e05
leafknode <leafknode@b10e9645-db3f-0410-a6c5-e135923ffca7>
2008-12-15 20:42:53 +0000

    [JCATAPULT-ANT Import] import

---
+++ libraries/crud/trunk/src/java/main/com/inversoft/crud/control/SearchResults.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2001-2008, Inversoft, All Rights Reserved
+ */
+package com.inversoft.crud.control;
+
+import org.jcatapult.mvc.result.control.AbstractComponentControl;
+import org.jcatapult.mvc.result.control.annotation.ControlAttribute;
+import org.jcatapult.mvc.result.control.annotation.ControlAttributes;
+
+/**
+ * <p>
+ * This is a control for a search result table, pagination and form for
+ * deleting entities.
+ * </p>
+ *
+ * @author  Brian Pontarelli
+ */
+@ControlAttributes(
+    required = {
+        @ControlAttribute(name = "name"),
+        @ControlAttribute(name = "properties")
+    },
+    optional = {
+        @ControlAttribute(name = "deletable", types = {Boolean.class, boolean.class})
+    }
+)
+public class SearchResults extends AbstractComponentControl {
+    /**
+     * Removes the properties attribute and re-adds it as an array of Strings by splitting it on commas.
+     */
+    @Override
+    protected void addAdditionalAttributes() {
+        String properties = (String) attributes.remove("properties");
+        attributes.put("properties", properties.split("\\W*,\\W*"));
+    }
+
+    /**
+     * @return  Null.
+     */
+    protected String startTemplateName() {
+        return null;
+    }
+
+    /**
+     * @return  search-results.ftl
+     */
+    protected String endTemplateName() {
+        return "search-results.ftl";
+    }
+}
