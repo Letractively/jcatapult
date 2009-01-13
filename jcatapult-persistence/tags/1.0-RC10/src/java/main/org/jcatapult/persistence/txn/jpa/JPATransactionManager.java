cd07dfd5c3edc80294ed1d0f5011aaa3a8ddf5e3
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2009-01-13 20:13:14 +0000

    Copy from trunk to tags/1.0-RC10

---
+++ jcatapult-persistence/tags/1.0-RC10/src/java/main/org/jcatapult/persistence/txn/jpa/JPATransactionManager.java
@@ -0,0 +1,79 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ */
+package org.jcatapult.persistence.txn.jpa;
+
+import javax.persistence.EntityManager;
+
+import org.jcatapult.persistence.txn.DefaultTransactionState;
+import org.jcatapult.persistence.txn.TransactionManager;
+import org.jcatapult.persistence.txn.TransactionResultProcessor;
+import org.jcatapult.persistence.txn.TransactionState;
+
+import com.google.inject.Inject;
+
+/**
+ * <p>
+ * This class provides a simple transaction management support
+ * by leveraging the current persistence context and starting
+ * transactions, but also by using the implementation of the
+ * {@link TransactionResultProcessor} that is passed to the end
+ * method to determine rollbacks.
+ * </p>
+ *
+ * <p>
+ * This is a not a singleton since it gets injected with the
+ * current EntityManager.
+ * </p>
+ *
+ * @author  Brian Pontarelli
+ */
+public class JPATransactionManager implements TransactionManager {
+    private final EntityManager entityManager;
+
+    @Inject
+    public JPATransactionManager(EntityManager entityManager) {
+        this.entityManager = entityManager;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public TransactionState startTransaction() {
+        javax.persistence.EntityTransaction txn = entityManager.getTransaction();
+        boolean embedded = txn.isActive();
+        if (!embedded) {
+            txn.begin();
+        }
+
+        return new DefaultTransactionState(txn, embedded);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public void endTransaction(Object result, Throwable t, TransactionState txn,
+            TransactionResultProcessor processor) {
+        boolean rollback = processor.rollback(result, t) || txn.transaction().getRollbackOnly();
+        if (rollback && !txn.embedded()) { // Not embedded, roll back
+            txn.transaction().rollback();
+        } else if (rollback) { // Embedded, set flag
+            txn.transaction().setRollbackOnly();
+        } else if (!txn.embedded()) { // Not embedded and success
+            txn.transaction().commit();
+        } // Embedded and success
+    }
+}
