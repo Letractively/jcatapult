a176f300a5e59683296b9222d3af4de66eddf7c1
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2009-01-13 20:59:46 +0000

    Copy from trunk to tags/1.0-RC10

---
+++ tools/jcatapult-deployer/tags/1.0-RC10/src/java/main/org/jcatapult/deployer/net/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * <p>
+ * This package contains APIs necessary to perform SCP and SSH Execs processes
+ * </p>
+ */
+package org.jcatapult.deployer.net;
---
+++ tools/jcatapult-deployer/tags/1.0-RC10/src/java/main/org/jcatapult/deployer/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * <p>
+ * This package contains the application managers and the deployer interface
+ * used by deployer.groovy scripts to deploy resources to domain environments
+ * </p>
+ */
+package org.jcatapult.deployer;
