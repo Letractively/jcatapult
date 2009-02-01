78b4be455b8bce9ffcd67e04222732da4d5a50a9
leafknode <leafknode@b10e9645-db3f-0410-a6c5-e135923ffca7>
2009-02-01 01:21:38 +0000

    added uber alpha version of JPA to DSL API

---
+++ jcatapult-dbmgr/trunk/src/java/main/org/jcatapult/dbmgr/dsl/hibernate/HibernateJPADSLGenerator.java
@@ -0,0 +1,17 @@
+package org.jcatapult.dbmgr.dsl.hibernate;
+
+import org.jcatapult.dbmgr.dsl.JPAAnnotationMapper;
+import org.jcatapult.dbmgr.dsl.JPADSLGenerator;
+
+/**
+ * @author jhumphrey
+ */
+public class HibernateJPADSLGenerator implements JPADSLGenerator {
+
+    /**
+     * {@inheritDoc}
+     */
+    public String generate(JPAAnnotationMapper processor) {
+        return null;
+    }
+}
