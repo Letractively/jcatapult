7a9be0911f70f5c494565a6fc32a26dbec2b1df7
leafknode <leafknode@b10e9645-db3f-0410-a6c5-e135923ffca7>
2009-02-03 20:04:29 +0000

    decided it wasn't needed and overkill

--- tools/jcatapult-deployer/tags/1.0-RC10/src/java/main/org/jcatapult/deployer/net/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * <p>
- * This package contains APIs necessary to perform SCP and SSH Execs processes
- * </p>
- */
-package org.jcatapult.deployer.net;
--- tools/jcatapult-deployer/tags/1.0-RC2/src/java/main/org/jcatapult/deployer/service/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * <p>
- * This package contains the service APIs for the jcatapult deployer framework
- * </p>
- */
-package org.jcatapult.deployer.service;
