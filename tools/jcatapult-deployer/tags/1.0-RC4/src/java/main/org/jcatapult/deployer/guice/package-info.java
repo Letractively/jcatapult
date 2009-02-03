c2172cc53d4fbaffc35a550d55ede529e29fa756
leafknode <leafknode@b10e9645-db3f-0410-a6c5-e135923ffca7>
2009-02-03 20:04:29 +0000

    decided it wasn't needed and overkill

--- tools/jcatapult-deployer/tags/1.0-RC4/src/java/main/org/jcatapult/deployer/guice/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * <p>
- * Contains the Guice module to bind interfaces to concrete implementation for dependency injection
- * </p>
- */
-package org.jcatapult.deployer.guice;
--- tools/jcatapult-deployer/tags/1.0-RC8/src/java/main/org/jcatapult/deployer/domain/package-info.java
+++
@@ -1,6 +0,0 @@
-/**
- * <p>
- * This package contains all domain objects used by the org.jcatapult.deployer.service services
- * </p>
- */
-package org.jcatapult.deployer.domain;
