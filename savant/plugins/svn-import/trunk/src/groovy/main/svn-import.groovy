88b9e17bdc935fac9d73bec6fbff235876a122f6
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2009-03-24 15:37:37 +0000

    Added rename, check out, property set.

--- savant/plugins/svn-import/trunk/src/groovy/main/svn-import.groovy
+++ savant/plugins/svn-import/trunk/src/groovy/main/svn-import.groovy
@@ -11,6 +11,9 @@ import org.tmatesoft.svn.core.wc.SVNWCUtil
 import org.tmatesoft.svn.core.internal.io.dav.DAVRepositoryFactory
 import org.tmatesoft.svn.core.internal.io.svn.SVNRepositoryFactoryImpl
 import org.tmatesoft.svn.core.internal.io.fs.FSRepositoryFactory
+import org.tmatesoft.svn.core.wc.SVNRevision
+import org.tmatesoft.svn.core.SVNPropertyValue
+import org.tmatesoft.svn.core.wc.ISVNPropertyHandler
 
 println """svn-import is an interactive tool to help you import a local project directory into a remote subversion
 repository. Please ensure that you are NOT in the project directory currently. If you are in the
@@ -39,7 +42,7 @@ if (!projectDir.isDirectory()) {
 }
 
 println 'Enter the SubVersion URL (excluding the project name)'
-def url = reader.readLine()
+def url = reader.readLine() + "/" + projectName
 
 println 'Enter the SubVersion username'
 def username = reader.readLine()
@@ -50,7 +53,7 @@ def password = reader.readLine()
 def projectPath = projectDir.getAbsolutePath()
 def confirm = ''
 while (confirm != 'y' && confirm != 'n') {
-  println "Really import [${projectPath}] to [${url}/${projectName}]? (y or n)"
+  println "Really import [${projectPath}] to [${url}]? (y or n)"
   confirm = reader.readLine()
   if (confirm == 'n') {
     System.exit(0)
@@ -66,7 +69,7 @@ projectDir.eachFileMatch(/.+\.iws/) {
   this.delete()
 }
 
-println "Importing [${projectPath}] to [${url}/${projectName}/trunk]..."
+println "Importing [${projectPath}] to [${url}/trunk]..."
 
 try {
   // Setup the SubVersion handlers
@@ -87,7 +90,15 @@ try {
   SVNURL branches = svnURL.appendPath("branches", false)
   SVNURL tags = svnURL.appendPath("tags", false)
   client.doMkDir([branches, tags] as SVNURL[], "Branches and tags")
+
+  // Move the project to the backup and check out the project
+  if (projectDir.renameTo(new File(jcatDir))) {
+    clientManager.getUpdateClient().doCheckout(trunk, projectDir, SVNRevision.HEAD, SVNRevision.HEAD, SVNDepth.INFINITY, false)
+    clientManager.getWCClient().doSetProperty(projectDir, "svn:ignore", SVNPropertyValue.create("target\n*.iws"), true, SVNDepth.EMPTY, ISVNPropertyHandler.NULL, null)
+  } else {
+    println "Unable to move the existing project directory out of the way and checkout from SubVersion. You'll have to do that manually."
+  }
 } catch (SVNException e) {
   e.printStackTrace()
-  throw new SavantBuildException("Failure while importing project", e);
+  throw new SavantBuildException("Failure while importing project", e)
 }
