777a7b01fb1e2dd2e553ac487023acf5c01c7453
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2009-04-13 22:55:59 +0000

    Copy from trunk to tags/1.0-RC13

---
+++ libraries/jcatapult-filemgr/tags/1.0-RC13/src/java/test/unit/org/jcatapult/filemgr/BaseTest.java
@@ -0,0 +1,13 @@
+package org.jcatapult.filemgr;
+
+import org.jcatapult.test.JCatapultBaseTest;
+import org.junit.Ignore;
+
+/**
+ * User: jhumphrey
+ * Date: Mar 28, 2008
+ */
+@Ignore
+public class BaseTest extends JCatapultBaseTest {
+    protected final String testDir = System.getProperty("user.home") + "/.jcatapult/testing-filemgr";
+}
