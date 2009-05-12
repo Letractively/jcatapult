42fe5f54234fced110ba647f80f78f88924553dc
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2009-05-12 20:32:42 +0000

    Copy from trunk to tags/1.0

---
+++ jcatapult-security/tags/1.0/src/java/main/org/jcatapult/security/guice/SecurityModule.java
@@ -0,0 +1,73 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.jcatapult.security.guice;
+
+import org.jcatapult.security.EnhancedSecurityContext;
+import org.jcatapult.security.SecurityContext;
+import org.jcatapult.security.UserAdapter;
+import org.jcatapult.security.login.AuthenticationService;
+import org.jcatapult.security.servlet.JCatapultSecurityContextProvider;
+import org.jcatapult.security.spi.EnhancedSecurityContextProvider;
+import org.jcatapult.security.spi.SecurityContextProvider;
+
+import com.google.inject.AbstractModule;
+
+/**
+ * <p>
+ * This class is just a helper class that web applications can extend to
+ * ensure that they have defined the correct classes for use with the
+ * JCatapult security framework.
+ * </p>
+ *
+ * @author  Brian Pontarelli
+ */
+public abstract class SecurityModule extends AbstractModule {
+    protected void configure() {
+        configureImplementations();
+        configureContexts();
+    }
+
+    /**
+     * Configures the implementations of the security frameworks interfaces that the application
+     * must provide via the abstract methods of this class.
+     */
+    protected void configureImplementations() {
+        // Set the sub-class security implementations
+        bind(AuthenticationService.class).to(getAuthenticationService());
+        bind(UserAdapter.class).to(getUserAdapter());
+    }
+
+    /**
+     * Configure the SecurityContext ({@link SecurityContext} and {@link EnhancedSecurityContext}).
+     */
+    protected void configureContexts() {
+        // Static inject the contexts
+        bind(SecurityContextProvider.class).to(JCatapultSecurityContextProvider.class);
+        requestStaticInjection(SecurityContext.class);
+        bind(EnhancedSecurityContextProvider.class).to(JCatapultSecurityContextProvider.class);
+        requestStaticInjection(EnhancedSecurityContext.class);
+    }
+
+    /**
+     * @return  The authentication service implementation class.
+     */
+    protected abstract Class<? extends AuthenticationService> getAuthenticationService();
+
+    /**
+     * @return  The user adapter implementation class.
+     */
+    protected abstract Class<? extends UserAdapter> getUserAdapter();
+}
