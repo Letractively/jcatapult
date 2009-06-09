1a9a29864160a9df46cbd8612b1f2a57a216578c
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2009-06-09 20:54:34 +0000

    Copy from trunk to tags/1.0

---
+++ libraries/jcatapult-filemgr/tags/1.0/src/java/main/org/jcatapult/filemgr/domain/fck/FileData.java
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2001-2007, Inversoft, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+
+package org.jcatapult.filemgr.domain.fck;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+
+/**
+ * <p>
+ * This class represents a single File for a listing action from the client.
+ * This is embedded in the {@link org.jcatapult.filemgr.domain.Connector} XML class, which defines a XML
+ * wrapper named <strong>Files</strong>.
+ * </p>
+ *
+ * @author  Brian Pontarelli
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+public class FileData {
+    @XmlAttribute()
+    private String name;
+    @XmlAttribute()
+    private long size;
+
+    public FileData() {
+    }
+
+    public FileData(String name, long size) {
+        this.name = name;
+        this.size = size;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public long getSize() {
+        return size;
+    }
+
+    public void setSize(long size) {
+        this.size = size;
+    }
+}
