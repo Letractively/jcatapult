bf46f2a8eb11a373121005636c956d446cfa9499
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2010-02-05 20:48:38 +0000

    Copy from trunk to tags/1.0.5

---
+++ jcatapult-persistence/tags/1.0.5/src/java/main/org/jcatapult/persistence/txn/TransactionState.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ *
+ */
+package org.jcatapult.persistence.txn;
+
+import javax.persistence.EntityTransaction;
+
+/**
+ * <p>
+ * This interface is passed between the methods of the {@link TransactionManager}
+ * by the JCatapult transaction AOP support. You can implement this however
+ * you need and your TransactionManager is guaranteed to always get back what
+ * it instantiated. So, feel free to add more properties and cast away.
+ * </p>
+ *
+ * @author  Brian Pontarelli
+ */
+public interface TransactionState {
+    /**
+     * @return  The EntityTransaction that is created by the {@link TransactionManager}.
+     */
+    EntityTransaction transaction();
+
+    /**
+     * @return  True if the transaction is embedded, false otherwise.
+     */
+    boolean embedded();
+}
