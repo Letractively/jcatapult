e327c44233681b3637d904475771b51f0e5449dd
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2010-07-02 03:16:50 +0000

    Copy from trunk to tags/1.0.14

---
+++ jcatapult-mvc/tags/1.0.14/src/java/main/org/jcatapult/mvc/action/result/AbstractResult.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.jcatapult.mvc.action.result;
+
+import java.lang.annotation.Annotation;
+import javax.servlet.http.HttpServletRequest;
+
+import org.jcatapult.mvc.action.ActionInvocation;
+import org.jcatapult.mvc.parameter.el.ExpressionEvaluator;
+
+import com.google.inject.Inject;
+
+/**
+ * <p>
+ * This result performs a servlet forward to a JSP or renders a FreeMarker
+ * template depending on the extension of the page.
+ * </p>
+ *
+ * @author  Brian Pontarelli
+ */
+public abstract class AbstractResult<U extends Annotation> implements Result<U> {
+    protected final ExpressionEvaluator expressionEvaluator;
+
+    @Inject
+    protected AbstractResult(ExpressionEvaluator expressionEvaluator) {
+        this.expressionEvaluator = expressionEvaluator;
+    }
+
+    /**
+     * If the action invocation isn't null, this returns an instance of the {@link ResultHttpServletRequest}
+     * class.
+     *
+     * @param   invocation The action invocation.
+     * @param   request The request.
+     * @return  The wrapped request or the request passed in, depending.
+     */
+    protected HttpServletRequest wrapRequest(ActionInvocation invocation, HttpServletRequest request) {
+        if (invocation.action() != null) {
+            return new ResultHttpServletRequest(request, invocation.action(), expressionEvaluator);
+        }
+
+        return request;
+    }
+
+    /**
+     * Expands any variables in the String.
+     *
+     * @param   str The String to expand.
+     * @param   action The action used to expand.
+     * @return  The result.
+     */
+    protected String expand(String str, Object action) {
+        if (action != null) {
+            return expressionEvaluator.expand(str, action);
+        }
+
+        return str;
+    }
+}
