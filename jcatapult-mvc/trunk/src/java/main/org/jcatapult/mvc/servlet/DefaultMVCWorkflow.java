3e5590ea4e07ded594c897024bd0ee889f4dc63b
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2010-08-09 20:27:07 +0000

    Removed file and body handling. That is now in core. Bumped core version

--- jcatapult-mvc/trunk/src/java/main/org/jcatapult/mvc/servlet/DefaultMVCWorkflow.java
+++ jcatapult-mvc/trunk/src/java/main/org/jcatapult/mvc/servlet/DefaultMVCWorkflow.java
@@ -28,7 +28,6 @@ import org.jcatapult.mvc.action.result.ResultInvocationWorkflow;
 import org.jcatapult.mvc.message.MessageWorkflow;
 import org.jcatapult.mvc.parameter.ParameterWorkflow;
 import org.jcatapult.mvc.parameter.URIParameterWorkflow;
-import org.jcatapult.mvc.parameter.RequestBodyWorkflow;
 import org.jcatapult.mvc.scope.ScopeRetrievalWorkflow;
 import org.jcatapult.mvc.scope.ScopeStorageWorkflow;
 import org.jcatapult.mvc.validation.ValidationWorkflow;
@@ -48,8 +47,7 @@ public class DefaultMVCWorkflow implements MVCWorkflow {
     private List<Workflow> workflows;
 
     @Inject
-    public DefaultMVCWorkflow(RequestBodyWorkflow requestBodyWorkflow,
-                              ActionMappingWorkflow actionMappingWorkflow,
+    public DefaultMVCWorkflow(ActionMappingWorkflow actionMappingWorkflow,
                               ScopeRetrievalWorkflow scopeRetrievalWorkflow,
                               MessageWorkflow messageWorkflow,
                               URIParameterWorkflow uriParameterWorkflow,
@@ -59,7 +57,7 @@ public class DefaultMVCWorkflow implements MVCWorkflow {
                               ActionInvocationWorkflow actionInvocationWorkflow,
                               ScopeStorageWorkflow scopeStorageWorkflow,
                               ResultInvocationWorkflow resultInvocationWorflow) {
-        workflows = asList(requestBodyWorkflow,actionMappingWorkflow, scopeRetrievalWorkflow, messageWorkflow, 
+        workflows = asList(actionMappingWorkflow, scopeRetrievalWorkflow, messageWorkflow, 
             uriParameterWorkflow, actionPrepareWorkflow, parameterWorkflow, validationWorkflow,
             actionInvocationWorkflow, scopeStorageWorkflow, resultInvocationWorflow);
     }
