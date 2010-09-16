fda6a195f9150b1a5070416fd14f11b6392fa392
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2010-09-16 22:05:33 +0000

    Added clear

--- jcatapult-core/trunk/src/java/test/unit/org/jcatapult/test/servlet/MockHttpSession.java
+++ jcatapult-core/trunk/src/java/test/unit/org/jcatapult/test/servlet/MockHttpSession.java
@@ -16,12 +16,12 @@
  */
 package org.jcatapult.test.servlet;
 
-import java.util.Enumeration;
-import java.util.HashMap;
-import java.util.Map;
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpSession;
 import javax.servlet.http.HttpSessionContext;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
 
 import net.java.util.IteratorEnumeration;
 
@@ -104,5 +104,9 @@ public class MockHttpSession implements HttpSession {
 
     public boolean isNew() {
         return false;
+    }
+
+    public void clear() {
+        attributes.clear();
     }
 }
