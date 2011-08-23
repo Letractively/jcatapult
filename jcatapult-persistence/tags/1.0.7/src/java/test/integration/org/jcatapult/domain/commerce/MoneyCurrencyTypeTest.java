f4e599b65c3ede448582db11e3e845c49a341253
bpontarelli <bpontarelli@b10e9645-db3f-0410-a6c5-e135923ffca7>
2011-08-23 19:15:35 +0000

    Copy from trunk to tags/1.0.7

---
+++ jcatapult-persistence/tags/1.0.7/src/java/test/integration/org/jcatapult/domain/commerce/MoneyCurrencyTypeTest.java
@@ -0,0 +1,48 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.jcatapult.domain.commerce;
+
+import java.math.BigDecimal;
+import java.util.Currency;
+import java.util.List;
+
+import org.jcatapult.persistence.service.PersistenceService;
+import org.jcatapult.persistence.test.JPABaseTest;
+import org.junit.Test;
+
+import com.google.inject.Inject;
+import static org.junit.Assert.*;
+
+/**
+ * <p> This class tests the hibernate types. </p>
+ *
+ * @author Brian Pontarelli
+ */
+public class MoneyCurrencyTypeTest extends JPABaseTest {
+  @Inject public PersistenceService persistenceService;
+
+  @Test
+  public void save() {
+    MoneyHolder holder = new MoneyHolder();
+    holder.setMoney(Money.valueOf("1.99", Currency.getInstance("EUR")));
+    persistenceService.persist(holder);
+
+    List<MoneyHolder> holders = persistenceService.findAllByType(MoneyHolder.class);
+    assertEquals(1, holders.size());
+    assertEquals(new BigDecimal("1.99"), holders.get(0).getMoney().toBigDecimal());
+    assertEquals(Currency.getInstance("EUR"), holders.get(0).getMoney().getCurrency());
+  }
+}
---
+++ jcatapult-persistence/tags/1.0.7/src/java/test/integration/org/jcatapult/persistence/PersistenceBaseTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2001-2007, JCatapult.org, All Rights Reserved
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND,
+ * either express or implied. See the License for the specific
+ * language governing permissions and limitations under the License.
+ */
+package org.jcatapult.persistence;
+
+import org.jcatapult.persistence.service.PersistenceService;
+import org.jcatapult.persistence.test.JPABaseTest;
+import org.junit.Ignore;
+
+import com.google.inject.Inject;
+
+/**
+ * <p> This class is the base test for jcatapult core to setup the test authentication service. </p>
+ *
+ * @author Brian Pontarelli
+ */
+@Ignore
+public class PersistenceBaseTest extends JPABaseTest {
+  @Inject public PersistenceService persistenceService;
+}
